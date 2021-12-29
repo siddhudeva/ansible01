7289dffd51e266b26077bb3590eb969ad0eb327e
siddhu <siddhudangeti@gmail.com>
2021-12-29 21:15:21 +0530

    Jenkins files

---
+++ jenkins/seed.groovy
@@ -0,0 +1,40 @@
+freeStyleJob('Roboshop') {
+    logRotator(-1, 10)
+    jdk('Java 8')
+    scm {
+        github('jenkinsci/job-dsl-plugin', 'master')
+    }
+    triggers {
+        githubPush()
+    }
+    steps {
+        gradle('clean build')
+    }
+    publishers {
+        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
+    }
+}
+folder('CI-pipeline') {
+    displayName('CI-pipeline')
+    description('CI-pipeline')
+}
+pipelineJob('CI-pipeline/cart') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/siddhudeva/cart.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
