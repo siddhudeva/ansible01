542997c6fa5348805b03168bce6a9d791e213490
siddhu <siddhudangeti@gmail.com>
2022-01-02 21:49:26 +0530

    sonarqube setup

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,4 @@
 def sonarQube() {
-    sh 'echo sonar qube on board'
+    sh 'sh sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.12.143:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+//            '  sh \'sonar-quality-gate.sh admin admin123 172.31.12.143 ${COMPONENT}\''
 }
