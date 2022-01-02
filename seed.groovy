895c0c822122cb6653c7281c3c2d608a7fc90010
siddhu <siddhudangeti@gmail.com>
2022-01-02 16:16:22 +0530

    NEW SETUP FOR SONARWQUBE

--- seed.groovy
+++ seed.groovy
@@ -18,25 +18,28 @@ folder('CI-pipeline') {
     displayName('CI-pipeline')
     description('CI-pipeline')
 }
-parameters { text(name: 'COMPONENT', defaultValue: 'user\ncatalogue\ncart\npayment', description: '') }
-
-pipelineJob('CI-pipeline/${COMPONENT}') {
-    configure { flowdefinition ->
-        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-                'userRemoteConfigs' {
-                    'hudson.plugins.git.UserRemoteConfig' {
-                        'url'('https://github.com/siddhudeva/${COMPONENT}.git')
+def COMPONENTS = ["cart", "catalogue", "payment", "shipping", "user", "dispatch"]
+def SIZE =  COMPONENTS.size -1
+for( i in 0..SIZE) {
+    def j = COMPONENTS[i]
+    pipelineJob('CI-pipeline/${j}') {
+        configure { flowdefinition ->
+            flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+                'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/siddhudeva/${j}.git')
+                        }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
+                        }
                     }
                 }
-                'branches' {
-                    'hudson.plugins.git.BranchSpec' {
-                        'name'('*/main')
-                    }
-                }
+                'scriptPath'('Jenkinsfile')
+                'lightweight'(true)
             }
-            'scriptPath'('Jenkinsfile')
-            'lightweight'(true)
         }
     }
 }
