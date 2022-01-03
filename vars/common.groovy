43148789aaab6847da2f1be5c04be2ad13116ee4
siddhu <siddhudangeti@gmail.com>
2022-01-03 10:43:07 +0530

    sonarqube setup

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,4 +1,4 @@
 def sonarQube() {
-   sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.12.143:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+   sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.12.143:9000 -Dsonar.login=d88bdf934eacc465d4200a82cca6650da40d17f7'
 
 }
