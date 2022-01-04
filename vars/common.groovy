54639128ae5000b79ee670a9ae524d48c01b8e75
siddhu <siddhudangeti@gmail.com>
2022-01-04 15:09:15 +0530

    nodejs file updated

--- vars/common.groovy
+++ vars/common.groovy
@@ -7,11 +7,19 @@ def sonarQube() {
 
 def Pubishartifacts() {
   if(env.GIT_BRANCH == "*tag*") {
-    println 'Ran on Tag'
+    echo 'This is ran by tag'
   } else {
     Utils.markStageSkippedForConditional('publish artifactes')
   }
 }
+//
+//  if (env.GIT_BRANCH == 'master') {
+//  echo 'I only execute on the master branch'
+//} else {
+//  echo 'I execute elsewhere'
+//}
+
+
 
 //def Pubishatifacts() {
 //  if(env.GIT_BRANCH == "*tag*") {
