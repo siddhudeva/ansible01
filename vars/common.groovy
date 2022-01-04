50c4ae6ced64822e6765c2cfc87d07e38f983460
siddhu <siddhudangeti@gmail.com>
2022-01-04 12:26:52 +0530

    nodejs file updated

--- vars/common.groovy
+++ vars/common.groovy
@@ -11,6 +11,6 @@ def Pubishartifacts() {
     println '(This is ran on tag)'
   }
   else {
-    Utils.markStageSkippedForConditional('publish artifactes')
+    Utils.markStageSkippedForConditional('sample')
   }
 }
