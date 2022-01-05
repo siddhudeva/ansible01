638f7ddc7e613b1a86e89d78a1d6d40726500e35
siddhu <siddhudangeti@gmail.com>
2022-01-05 07:11:26 +0530

    nodejs file updated

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -57,11 +57,11 @@ def call() {
                     }
                 }
             }
-            post {
-                always {
-                    cleanWs()
-                }
-            }
+//            post {
+//                always {
+//                    cleanWs()
+//                }
+//            }
 
         }
     }
