bb601af86917209e91631dd502cfd0a7ee60dc9f
siddhu <siddhudangeti@gmail.com>
2022-01-05 11:42:29 +0530

    preparing and publishing artifacts

--- vars/common.groovy
+++ vars/common.groovy
@@ -14,7 +14,7 @@ def publishArtifacts() {
 //    Utils.markStageSkippedForConditional('Publish Artifacts')
 //  }
   sh '''
-curl -v -u admin:Admin123 --upload-file ${COMPONENT}.${gitTag}.zip http://3.237.181.62:8081/repository/{COMPONENT}/${COMPONENT}.${gitTag}.zip
+curl -v -u admin:Admin123 --upload-file ${COMPONENT}.${gitTag}.zip http://172.31.9.5:8081/repository/${COMPONENT}/${COMPONENT}.${gitTag}.zip
 '''
 
 }
