a31c2a92f1ab293907852857dd6149de59a6d0db
siddhu <siddhudangeti@gmail.com>
2022-01-06 22:26:08 +0530

    nginx file added

--- vars/common.groovy
+++ vars/common.groovy
@@ -14,7 +14,7 @@ def publishArtifacts() {
 //    Utils.mark  StageSkippedForConditional('Publish Artifacts')
 //  }
   sh '''
-curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}.${gitTag}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}.${gitTag}.zip
+curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ../${COMPONENT}.${gitTag}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}.${gitTag}.zip
 '''
 
 }
