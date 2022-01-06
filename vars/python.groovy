cfd5418eb57f63d1e7dd55c0eefa0ea1e1acf20a
siddhu <siddhudangeti@gmail.com>
2022-01-06 18:03:39 +0530

    payment setup for nexus repository

--- vars/python.groovy
+++ vars/python.groovy
@@ -61,12 +61,11 @@ def call() {
                     }
                 }
             }
-//            post {
-//                always {
-//                    cleanWs()
-//                }
-//            }
-
+        }
+        post {
+            always {
+                cleanWs()
+            }
         }
     }
 }
