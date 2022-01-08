b6b4fed39d179d57bddc744c27c67c8ec0d792b0
siddhu <siddhudangeti@gmail.com>
2022-01-08 16:42:07 +0530

    publishing artifactory file is updated

--- vars/common.groovy
+++ vars/common.groovy
@@ -14,6 +14,7 @@ def publishArtifacts() {
 //    Utils.mark  StageSkippedForConditional('Publish Artifacts')
 //  }
   sh '''
+    pwd
     curl -v -f -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}.${gitTag}.zip http://nexus1.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}.${gitTag}.zip
 '''
 
