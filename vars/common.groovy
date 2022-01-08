ab414029cb9deff4712832145be0fa084c3c62cb
siddhu <siddhudangeti@gmail.com>
2022-01-08 10:02:58 +0530

    jenkins pipeline files

--- vars/common.groovy
+++ vars/common.groovy
@@ -14,7 +14,7 @@ def publishArtifacts() {
 //    Utils.mark  StageSkippedForConditional('Publish Artifacts')
 //  }
   sh '''
-curl -f -v -a -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}.${gitTag}.zip http://3.231.107.170:8081/repository/${COMPONENT}/${COMPONENT}.${gitTag}.zip
+curl -F -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}.${gitTag}.zip http://3.231.107.170:8081/repository/${COMPONENT}/${COMPONENT}.${gitTag}.zip
 '''
 
 }
