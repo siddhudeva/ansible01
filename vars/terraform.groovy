7101679866c9530a9a628c3c6f24060aecfe65b6
siddhu <siddhudangeti@gmail.com>
2022-01-18 12:51:48 +0530

    seed.groovy file is added

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -37,7 +37,7 @@ def call() {
                 steps {
                     sh '''
                      terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
-                     terraform ${ACTION} -var-file=env/${ENVIRONMENT}.tfvars
+                     terraform ${ACTION} -auto-approve -var-file=env/${ENVIRONMENT}.tfvars
                    '''
                 }
             }
