3c91103bb76f01a0558343dab60226d18b0c4398
siddhu <siddhudangeti@gmail.com>
2022-01-18 15:04:35 +0530

    TERRAFORM FILE REVISITED

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -8,15 +8,15 @@ def call() {
             choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'Pick action to perform')
 
         }
-        stages {
-            stage('Label Builds') {
-                steps {
-                    script {
-                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${ENVIRONMENT}"
-                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${ACTION}"
-
-                    }
-                }
+//        stages {
+//            stage('Label Builds') {
+//                steps {
+//                    script {
+//                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${ENVIRONMENT}"
+//                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${ACTION}"
+//
+//                    }
+//                }
             }
             stage('Apply terraform actions') {
                 steps {
@@ -35,6 +35,4 @@ def call() {
 
             }
         }
-    }
 
-}
