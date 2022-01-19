e20125452d7b6bb407f1c5c53d05333b6f38eb76
siddhu <siddhudangeti@gmail.com>
2022-01-19 16:46:13 +0530

    seed.groovy file modified with Mutable/Infra

--- seed.groovy
+++ seed.groovy
@@ -103,30 +103,30 @@
 //}
 
 
-//folder('Mutable') {
-//    displayName('Mutable')
-//    description('Mutable')
-//}
-//pipelineJob('Mutable/VPC') {
-//    configure { flowdefinition ->
-//        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//                'userRemoteConfigs' {
-//                    'hudson.plugins.git.UserRemoteConfig' {
-//                        'url'('https://github.com/siddhudeva/terraform-vpc.git')
-//                    }
-//                }
-//                'branches' {
-//                    'hudson.plugins.git.BranchSpec' {
-//                        'name'('*/main')
-//                    }
-//                }
-//            }
-//            'scriptPath'('Jenkinsfile')
-//            'lightweight'(true)
-//        }
-//    }
-//}
+folder('Mutable') {
+    displayName('Mutable')
+    description('Mutable')
+}
+pipelineJob('Mutable/VPC') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/siddhudeva/terraform-vpc.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
 
 
 pipelineJob('Mutable/APP-SETUP') {
