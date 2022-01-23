8a6bb3d7a6d017cae79d9117c6740434324242e9
siddhu <siddhudangeti@gmail.com>
2022-01-23 09:45:19 +0530

    seed.groovy

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -62,11 +62,11 @@ def call() {
                     }
                 }
             }
-//            post {
-//                always {
-//                    cleanWs()
-//                }
-//            }
+            post {
+                always {
+                    cleanWs()
+                }
+            }
 
         }
     }
