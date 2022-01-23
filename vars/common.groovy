e1d32f2e739c6f8a8ec4a5da6be0ca0825914416
siddhu <siddhudangeti@gmail.com>
2022-01-23 09:51:35 +0530

    nodejs.groovy file updated

--- vars/common.groovy
+++ vars/common.groovy
@@ -23,6 +23,7 @@ def publishArtifacts() {
 def preparingArtifacts() {
   if(env.PROG_LANG_NAME == "nodejs" && env.PROG_LANG_VERSION == "6") {
     sh '''
+       pwd
        npm install
        zip -r ${COMPONENT}.${gitTag}.zip node_modules server.js
 '''
