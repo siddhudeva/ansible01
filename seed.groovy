e1f7bfb131a6b33ffd9beb9f979ca0b41eba063c
siddhu <siddhudangeti@gmail.com>
2022-01-23 08:19:14 +0530

    seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,40 +1,40 @@
-//
-//folder('CI-Pipelines') {
-//    displayName('CI-Pipelines')
-//    description('CI-Pipelines')
-//}
-//
-//def COMPONENTS = ["cart", "catalogue", "payment", "shipping", "user", "dispatch"]
-//
-//def SIZE =  COMPONENTS.size -1
-//
-//for(i in 0..SIZE) {
-//    def j = COMPONENTS[i]
-//    pipelineJob("CI-Pipelines/${j}") {
-//        configure { flowdefinition ->
-//            flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
-//                'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
-//                    'userRemoteConfigs' {
-//                        'hudson.plugins.git.UserRemoteConfig' {
-//                            'url'("https://github.com/siddhudeva/${j}.git")
-//                        }
-//                    }
-//                    'branches' {
-//                        'hudson.plugins.git.BranchSpec' {
-//                            'name'('*/main')
-//                        }
-//                    }
-//                }
-//                'scriptPath'('Jenkinsfile')
-//                'lightweight'(true)
-//            }
-//        }
-//    }
-//}
-//
-//
-//
-//
+
+folder('CI-Pipelines') {
+    displayName('CI-Pipelines')
+    description('CI-Pipelines')
+}
+
+def COMPONENTS = ["cart", "catalogue", "payment", "shipping", "user", "dispatch"]
+
+def SIZE =  COMPONENTS.size -1
+
+for(i in 0..SIZE) {
+    def j = COMPONENTS[i]
+    pipelineJob("CI-Pipelines/${j}") {
+        configure { flowdefinition ->
+            flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+                'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'("https://github.com/siddhudeva/${j}.git")
+                        }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
+                        }
+                    }
+                }
+                'scriptPath'('Jenkinsfile')
+                'lightweight'(true)
+            }
+        }
+    }
+}
+
+
+
+
 ////freeStyleJob('Roboshop') {
 ////    logRotator(-1, 10)
 ////    jdk('Java 8')
