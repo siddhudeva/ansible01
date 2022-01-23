19007ece4d8a5ba48da83e51d0587770930394d4
siddhu <siddhudangeti@gmail.com>
2022-01-23 13:11:26 +0530

    terraform.groovy file is updated

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -22,7 +22,7 @@ def call() {
                 steps {
                     script {
                         sh '''
-                          terraform init -backend-config=./env/${ENVIRONMENT}-backend.tfvars  
+                          terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars  
                            terraform ${ACTION} -auto-approve -var-file=env/${ENVIRONMENT}.tfvars
                        '''
                     }
