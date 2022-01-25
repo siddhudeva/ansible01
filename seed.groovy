0afa5ba5a877e08b87849cbe819217d8d7889ed1
siddhu <siddhudangeti@gmail.com>
2022-01-25 16:19:31 +0530

    VPC - Terraform databases added rabbitmq and mongodb

--- seed.groovy
+++ seed.groovy
@@ -170,3 +170,25 @@ pipelineJob('Mutable/APP-ALB') {
         }
     }
 }
+
+
+pipelineJob('VPC/Terraform-Databases') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/siddhudeva/terraform_databases.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
