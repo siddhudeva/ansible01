1a1660f2dbf8a4913713c6076e3bd7c4fb65bfc4
siddhu <siddhudangeti@gmail.com>
2022-01-26 08:57:57 +0530

    jenkins file updated for Loadbalancer post function added

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -26,6 +26,7 @@ def call() {
                 steps {
                     script {
                         sh '''
+                           git clone https://github.com/siddhudeva/terraform-vpc.git
                            terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars  
                            terraform ${ACTION} -auto-approve -var-file=env/${ENVIRONMENT}.tfvars
                        '''
