cf9b5a0d5796d538935395f1823a9fa8342ad1a1
siddhu <siddhudangeti@gmail.com>
2022-01-26 09:24:19 +0530

    Mutable/VPC file is updated

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,44 +1,93 @@
+//def call() {
+//    pipeline {
+//        agent {
+//            label "${BUILD_LABEL}"
+//        }
+//        options {
+//            ansiColor('xterm')
+//        }
+//
+//        parameters {
+//            choice(name: 'ENVIRONMENT', choices: ['', 'dev', 'prod'], description: 'Pick environment')
+//            choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'Pick action to perform')
+//
+//        }
+//        stages {
+//            stage('Label Builds') {
+//                steps {
+//                    script {
+//                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${ENVIRONMENT}"
+//                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${ACTION}"
+//
+//                    }
+//                }
+//            }
+//            stage('Apply terraform actions') {
+//                steps {
+//                    script {
+//                        sh '''
+//                           terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
+//                           terraform ${ACTION} -auto-approve -var-file=env/${ENVIRONMENT}.tfvars
+//                       '''
+//                    }
+//                }
+//            }
+//        }
+////        post {
+////            always {
+////                cleanWs()
+////
+////            }
+////        }
+//    }
+//
+//}
+
+
 def call() {
     pipeline {
         agent {
             label "${BUILD_LABEL}"
         }
+
+        //environment {}
+
         options {
             ansiColor('xterm')
         }
 
         parameters {
-            choice(name: 'ENVIRONMENT', choices: ['', 'dev', 'prod'], description: 'Pick environment')
-            choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'Pick action to perform')
-
+            choice(name: 'ENVIRONMENT', choices: ['', 'dev', 'prod'], description: 'Pick Environment')
+            choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'Pick Terraform Action')
         }
+
         stages {
+
             stage('Label Builds') {
                 steps {
                     script {
                         addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${ENVIRONMENT}"
                         addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${ACTION}"
-
                     }
                 }
             }
-            stage('Apply terraform actions') {
+
+            stage('Apply Terraform Action') {
                 steps {
-                    script {
-                        sh '''
-                           terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars  
-//                           terraform ${ACTION} -auto-approve -var-file=env/${ENVIRONMENT}.tfvars
-                       '''
-                    }
+                    sh '''
+            terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
+            terraform ${ACTION} -auto-approve -var-file=env/${ENVIRONMENT}.tfvars
+          '''
                 }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
             }
         }
-//        post {
-//            always {
-//                cleanWs()
-//
-//            }
-//        }
-    }
 
+    }
 }
